0aaea8b227f3be346d4aab8521df17b47bd16ac7
Mufassir-Chowdhury <mac22214u@gmail.com>
2022-04-10 02:00:53 +0600

    icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,7 +13,6 @@
 	<div class="text-center p-8">
 		<div class="text-4xl my-4 font-semibold">About Us</div>
 		We’re a group, enthusiastic about multiple fields, but focusing primarily on our common interest in security research. Security research is a niche subject not commonly pursued in Bangladesh, we plan to take it mainstream and conduct meaningful high quality projects and publications.
-If you’re interested in our work and want to collaborate with us feel free to email us at - irfannafizislive@gmail.com
 
 	</div>
 </section>
