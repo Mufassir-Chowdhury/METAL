d2ed0e19d63e4c2c6633f16a06f292a5903dfc0a
Mufassir-Chowdhury <mac22214u@gmail.com>
2022-04-14 18:31:30 +0600

    made index.svelte responsive

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,11 +6,11 @@
 	<title>Home</title>
 </svelte:head>
 
-<section class="py-8 flex items-center">
-	<div class="w-7/12 flex justify-center p-8">
+<section class="py-8 flex flex-col md:flex-row items-center divide-y md:divide-y-0">
+	<div class="md:w-7/12 flex justify-center p-8">
 		<img  src="group.jpg" alt="group">
 	</div>
-	<div class="text-center w-5/12 p-8">
+	<div class="text-center md:w-5/12 p-8">
 		<div class="text-4xl my-4 font-semibold">About Us</div>
 		We’re a group, enthusiastic about multiple fields, but focusing primarily on our common interest in security research. Security research is a niche subject not commonly pursued in Bangladesh, we plan to take it mainstream and conduct meaningful high quality projects and publications.
 
