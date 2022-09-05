7a72a17a335ec68e406aba9f626960376b8c82ca
Irfan Nafiz Shahan <irfannafizislive@gmail.com>
2022-09-05 14:40:48 +0600

    Added "Independent Security Research Group"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
 	</div>
 	<div class="text-center md:w-5/12 p-8">
 		<div class="text-4xl my-4 font-semibold">About Us</div>
-		We’re a group, enthusiastic about multiple fields, but focusing primarily on our common interest in security research. Security research is a niche subject not commonly pursued in Bangladesh, we plan to take it mainstream and conduct meaningful high quality projects and publications.
+		We’re an independent security research group, enthusiastic about multiple fields, but focusing primarily on our common interest in computer security. Security research is a niche subject not commonly pursued in Bangladesh, we plan to take it mainstream and conduct meaningful high quality projects and publications.
 <!-- 	<br>
 	<br>
 		Join the "Introduction to CTF" workshop's <a href="https://forms.gle/rTu9a8tGt1VgzZFE9" class="text-blue-400">Beginner CTF Challenge!</a> -->
@@ -20,4 +20,4 @@
 </section>
 
 
-<!-- Ooooo shiny... nmoss{well_web_isnt_so_hard_is_it} -->
+<!-- Backup for later > nmoss{well_web_isnt_so_hard_is_it} -->
