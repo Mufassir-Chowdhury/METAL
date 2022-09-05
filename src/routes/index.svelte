d13cf38cee9957f9629eda30f39929ef9ebcab5e
Irfan Nafiz Shahan <irfannafizislive@gmail.com>
2022-09-05 10:13:37 +0600

    Commented out the CTF Link for future temp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,9 +13,9 @@
 	<div class="text-center md:w-5/12 p-8">
 		<div class="text-4xl my-4 font-semibold">About Us</div>
 		We’re a group, enthusiastic about multiple fields, but focusing primarily on our common interest in security research. Security research is a niche subject not commonly pursued in Bangladesh, we plan to take it mainstream and conduct meaningful high quality projects and publications.
+<!-- 	<br>
 	<br>
-	<br>
-		Join the "Introduction to CTF" workshop's <a href="https://forms.gle/rTu9a8tGt1VgzZFE9" class="text-blue-400">Beginner CTF Challenge!</a>
+		Join the "Introduction to CTF" workshop's <a href="https://forms.gle/rTu9a8tGt1VgzZFE9" class="text-blue-400">Beginner CTF Challenge!</a> -->
 	</div>
 </section>
 
